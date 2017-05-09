0952283cf50fb939afe9f0a86420e1a1f058e7ee
Jessica Gilligan <jessicagilligan@Jessicas-MacBook-Pro.local>
2017-05-09 14:01:35 -0400

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("veggie tofu cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("philly cheese steak cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("french onion provolone burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("fried green tomato and pimento cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("wimpy's today for tomorrow cheeseburger", false);
